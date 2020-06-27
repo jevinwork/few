ff64cd79a95e693659db7cbb5319a5db2a238124
ryan <dooioomoo@gmail.com>
2020-06-27 21:11:33 +0900

    update

--- src/gulpfiles/gulp-javascript.js
+++ src/gulpfiles/gulp-javascript.js
@@ -4,20 +4,22 @@ const build = require("./gulp-require");
 module.exports = {
   cleanFiles: [setting.js.exportPath.common + "**/*.debug.js"],
   commonJs: () => {
-    return build.gulp
-      .src(setting.js.importPath.common)
-      .pipe(build.plumber())
-      .pipe(build.concat("common.js"))
-      .pipe(
-        build.minify({
-          ext: {
-            src: ".debug.js",
-            min: ".min.js",
-          },
-          ignoreFiles: [".combo.js", ".min.js", "-min.js"],
-        })
-      )
-      .pipe(build.gulp.dest(setting.js.exportPath.common));
+    return (
+      build.gulp
+        .src(setting.js.importPath.common)
+        // .pipe(build.plumber())
+        .pipe(build.concat("common.js"))
+        .pipe(
+          build.minify({
+            ext: {
+              src: ".debug.js",
+              min: ".min.js",
+            },
+            ignoreFiles: [".combo.js", ".min.js", "-min.js"],
+          })
+        )
+        .pipe(build.gulp.dest(setting.js.exportPath.common))
+    );
   },
   footJs: () => {
     return build.gulp
